3d676a9da9ff02c515bfd2ce75b2f0ff56c8ed3a
Maciej Pelczar <maciej.pelczar@miquido.com>
2021-06-25 11:07:24 +0200

    formatting

--- examples/complete/main.tf
+++ examples/complete/main.tf
@@ -8,8 +8,8 @@ module "video-streaming" {
   stage     = "test"
   namespace = "test"
 
-  aws_region                           = "us-east-1"
-  aws_used_account_no                  = "134446265017"
+  aws_region          = "us-east-1"
+  aws_used_account_no = "134446265017"
 
   mediaconvert-endpoint                = "https://yk2lhke4b.mediaconvert.eu-central-1.amazonaws.com"
   submit-lambda-notification-webhook   = "https://cf74a368e3b9e2b4c8fbcfeee8770dde.m.pipedream.net"
