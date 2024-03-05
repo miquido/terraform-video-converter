3ee8a5aabc6b8e3eacc3da248324385a0e619402
Miquido Ops <mqtc@miquido.com>
2024-03-05 03:20:24 +0000

    build(deps): bump video-source::terraform-aws-s3-bucket
    Bumps [video-source::terraform-aws-s3-bucket](https://github.com/cloudposse/terraform-aws-s3-bucket) from 1.0.0 to 4.2.0.
    - [Release notes](https://github.com/cloudposse/terraform-aws-s3-bucket/releases)
    - [Commits](https://github.com/cloudposse/terraform-aws-s3-bucket/compare/1.0.0...4.2.0)

--- video-source-bucket.tf
+++ video-source-bucket.tf
@@ -8,7 +8,7 @@ module "video-source" {
   namespace = module.label.namespace
   tags      = module.label.tags
 
-  source                  = "git::https://github.com/cloudposse/terraform-aws-s3-bucket.git?ref=1.0.0"
+  source                  = "git::https://github.com/cloudposse/terraform-aws-s3-bucket.git?ref=4.2.0"
   enabled                 = true
   user_enabled            = false
   versioning_enabled      = true
