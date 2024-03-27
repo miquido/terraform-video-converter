678046645a0b3a4b30301fe5835ee01e9a24fc98
Miquido Ops <mqtc@miquido.com>
2024-03-27 03:20:14 +0000

    build(deps): bump video-cdn::terraform-aws-cloudfront-s3-cdn
    Bumps [video-cdn::terraform-aws-cloudfront-s3-cdn](https://github.com/cloudposse/terraform-aws-cloudfront-s3-cdn) from 0.85.0 to 0.94.0.
    - [Release notes](https://github.com/cloudposse/terraform-aws-cloudfront-s3-cdn/releases)
    - [Commits](https://github.com/cloudposse/terraform-aws-cloudfront-s3-cdn/compare/0.85.0...0.94.0)

--- video-cdn.tf
+++ video-cdn.tf
@@ -1,5 +1,5 @@
 module "video-cdn" {
-  source = "git::https://github.com/cloudposse/terraform-aws-cloudfront-s3-cdn.git?ref=0.85.0"
+  source = "git::https://github.com/cloudposse/terraform-aws-cloudfront-s3-cdn.git?ref=0.94.0"
 
   name                    = "video"
   namespace               = module.label.namespace
